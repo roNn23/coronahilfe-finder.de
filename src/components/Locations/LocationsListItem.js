5259c76c6dfa54ce5f525ffea39197718938d9c0
Ronny Neefe <ronny.neefe@aoe.com>
2020-03-24 21:58:59 +0100

    enhanced location data

--- src/components/Locations/LocationsListItem.js
+++ src/components/Locations/LocationsListItem.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const LocationsListItem = ({ title, description, phone, website }) => {
+const LocationsListItem = ({ title, city, region, description, phone, opening_hours, offers, website }) => {
   return (
     <li className="comp-locations__list-item">
       <h3>{title}</h3>
+      {city && <p className="badge badge-secondary mr-2">{city}</p>}
+      {region && <p className="badge badge-secondary mr-2">{region}</p>}
       {description && <p dangerouslySetInnerHTML={{ __html: description }}></p>}
-      {phone.length > 0 && <p>Telefon: {phone}</p>}
+      {opening_hours && (
+        <p>
+          <strong>Öffnungszeiten:</strong> {opening_hours}
+        </p>
+      )}
+
+      {offers && (
+        <p>
+          <strong>Angebote:</strong>{' '}
+          {offers.map((offer, index) => (
+            <span className="badge badge-secondary mr-2" key={index}>
+              {offer}
+            </span>
+          ))}
+        </p>
+      )}
+
+      {phone.length > 0 && (
+        <p>
+          <strong>Telefon:</strong> {phone}
+        </p>
+      )}
       {website && (
         <a href={website} target="_blank" rel="noopener noreferrer">
           {website}
@@ -18,6 +41,10 @@ const LocationsListItem = ({ title, description, phone, website }) => {
 
 LocationsListItem.propTypes = {
   title: PropTypes.string.isRequired,
+  city: PropTypes.string,
+  region: PropTypes.string,
+  opening_hours: PropTypes.string,
+  offers: PropTypes.array,
   description: PropTypes.string,
   phone: PropTypes.array,
   website: PropTypes.string,
