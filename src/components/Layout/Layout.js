d6986f1791876c5195a1d3d26db6e5ea11770213
Ronny Neefe <ronny.neefe@aoe.com>
2020-03-24 22:16:44 +0100

    added favicon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,11 @@ import { Helmet } from 'react-helmet'
 const Layout = ({ children }) => {
   return (
     <>
-      <Helmet></Helmet>
+      <Helmet>
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
+      </Helmet>
       <Header />
       <main>{children}</main>
       <Footer />
