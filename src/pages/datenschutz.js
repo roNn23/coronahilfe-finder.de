faefbaefb00e1b2686796a063eef1c897b8b156a
Rebecca Wulff <rebecca.wulff@gmx.de>
2020-03-24 22:22:26 +0100

    added basic text

--- src/pages/datenschutz.js
+++ src/pages/datenschutz.js
@@ -9,9 +9,35 @@ const DatenschutzPage = () => (
     <Content>
       <h1>Datenschutzerklärung</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias nulla iusto voluptas voluptatibus
-        perferendis dolor temporibus ipsa eius similique fugiat facere quas, explicabo ipsum reiciendis quos, velit
-        eaque dignissimos.
+        Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren
+        Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen) erhoben werden, erfolgt
+        dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht
+        an Dritte weitergegeben. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der
+        Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff
+        durch Dritte ist nicht möglich.Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten
+        durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird
+        hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im
+        Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+      </p>
+      <h2>Datenschutzerklärung für die Nutzung von Google Analytics</h2>
+      <p>
+        Diese Website nutzt Funktionen des Webanalysedienstes Google Analytics. Anbieter ist die Google Inc., 1600
+        Amphitheatre Parkway Mountain View, CA 94043, USA. Google Analytics verwendet so genannte „Cookies“. Das sind
+        Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie
+        ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel
+        an einen Server von Google in den USA übertragen und dort gespeichert. Mehr Informationen zum Umgang mit
+        Nutzerdaten bei Google Analytics finden Sie in der Datenschutzerklärung von
+        Google: [https://support.google.com/analytics/answer/6004245?hl=de](https://support.google.com/analytics/answer/6004245?hl=de)
+      </p>
+      <h2>Browser Plugin</h2>
+      <p>
+        Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern;
+        wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser
+        Website vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch den Cookie
+        erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google sowie die
+        Verarbeitung dieser Daten durch Google verhindern, indem Sie das unter dem folgenden Link verfügbare Browser-
+        Plugin herunterladen und
+        installieren: [https://tools.google.com/dlpage/gaoptout?hl=de](https://tools.google.com/dlpage/gaoptout?hl=de)
       </p>
     </Content>
   </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,62 @@ const IndexPage = () => {
           <AddressSearch onAddressFound={handleAddressFound} />
         </div>
         {address && <Locations lat={address.lat} lon={address.lon} />}
+        <p>Hallo und schön, dass du hier bist!</p>
+        <div className="boxed">
+          <strong>Finde Hilfesuchende in deinem Umkreis oder Hilfsangebote in einer bestimmten Stadt!</strong> Kennst du
+          eine Hilfeseite, die nicht in der Liste steht?{' '}
+          <a href="https://forms.gle/twyugJya2gfdEMDB6">Trage sie hier ein!</a>
+        </div>
+        <p>
+          Wir erleben gerade eine sehr intensive Zeit, wie wir sie so (zumindest in Europa) schon lange nicht mehr
+          durchstehen mussten. Das Corona-Virus hält die Welt in Atem, wir spüren aber auch eine unglaubliche Welle an
+          Solidarität. Überall sprießen Hilfsangebote aus dem Boden: Gemeinden, Schülerinnen und Schüler, Studierende
+          oder Nachbarn schließen sich zusammen, um für Personen der Risikogruppe einkaufen zu gehen, Erledigungen zu
+          machen oder einfach nur da zu sein, wenn Redebedarf besteht. Die meisten dieser Angebote sind spontan
+          entstanden und schwer zu finden. Vor allem, wenn man sich um eine Person in Bielefeld sorgt, die Hilfe
+          benötigt und selbst aber in München wohnt.
+        </p>
+        <div className="boxed">
+          <strong>Ziel dieser Seite ist es, eine Übersicht über alle Hilfsangebote in Deutschland zu bekommen.</strong>
+        </div>
+        <p>
+          Wir haben bereits angefangen, Facebook-Gruppen, Webseiten oder Portale zusammenzutragen. Nun sind wir auf eure
+          Mitarbeit angewiesen. <strong>Kennt ihr eine Seite, die in dieser Liste stehen sollte?</strong> Dann tragt sie
+          bitte <a href="https://forms.gle/twyugJya2gfdEMDB6">hier</a> ein. Wir werden sie so schnell wir möglich prüfen
+          und in die Liste aufnehmen.
+        </p>
+        <div className="boxed">
+          <strong>Wollt ihr helfen, oder benötigt ihr/ein Mensch in eurem Umfeld Hilfe?</strong>
+        </div>
+        <p>Durchsucht die Liste und meldet euch bei den Verantwortlichen vor Ort.</p>
+        <h2>Für Helfer: Bitte beachten!</h2>
+        <ul>
+          <li>Haltet die empfohlenen Hygiene-Anforderungen ein.</li>
+          <li>
+            Helft maximal 2 bis 3 Haushalten, um das Risiko möglichst gering zu halten. Achtet dabei auf kontaktlose
+            Übergabe von Einkäufen.
+          </li>
+          <li>
+            Es ist momentan nicht überall erlaubt, haushaltsfremde Kinder zu betreuen. Möchtet ihr Eltern helfen, dann
+            nur einem Haushalt in eurer direkten Nachbarschaft. Informiert euch vorher über rechtliche Einschränkungen.
+          </li>
+          <li>Bringt euch selbst nicht Gefahr.</li>
+          <li>Ist eine Telefonnummer angegeben aber keine Öffnungszeiten, ruft bitte nur tagsüber an.</li>
+          <li>
+            Die meisten Angebote basieren auf freiwilliger Arbeit. D.h. ihr bekommt keine Entlohnung für euer Engagement
+            - außer das unbezahlbare Gefühl, eine gute Tat vollbracht zu haben.
+          </li>
+          <li>Beachtet ansonsten auch die Vorgaben von den jeweiligen Organisatoren.</li>
+        </ul>
+        <h2>Weitere Informationen zu Covid-19</h2>
+        <ul>
+          <li>
+            <a href="https://www.infektionsschutz.de/coronavirus/">Bundeszentrale für Gesundheitliche Aufklärung</a>
+          </li>
+          <li>
+            <a href="https://www.bundesgesundheitsministerium.de/coronavirus.html/">Bundesministerium für Gesundheit</a>
+          </li>
+        </ul>
       </Content>
     </Layout>
   )
