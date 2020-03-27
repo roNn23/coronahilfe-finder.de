f9870884325cac00199fda92d31f9e28dda394ac
Ronny Neefe <ronny.neefe@aoe.com>
2020-03-27 19:31:07 +0100

    added htaccess

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,22 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-htaccess',
+      options: {
+        RewriteBase: '/',
+        https: true,
+        www: false,
+        SymLinksIfOwnerMatch: true,
+        host: 'coronahilfe-finder.de', // if 'www' is set to 'false', be sure to also remove it here!
+        ErrorDocument: `
+            ErrorDocument 401 /error_pages/401.html
+            ErrorDocument 404 /error_pages/404.html
+            ErrorDocument 500 /error_pages/500.html
+          `,
+        redirect: ['RewriteRule ^(.*)$ https://coronahilfe-finder.de/$1 [R=301,L]'],
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-transformer-sharp',
